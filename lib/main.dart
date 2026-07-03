23cbd54b5dc3e7e8d9ef91a01135b6482323d993
abderrahimayadi08-dotcom <abderrahimayadi08@gmail.com>
2026-07-03 20:57:09 +0200

    Add global error handler to diagnose startup crash

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:madrasa_app/app.dart';
 import 'package:madrasa_app/core/services/logger.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  FlutterError.onError = (details) {
+    Logger.error('Flutter error: ${details.exception}');
+    FlutterError.presentError(details);
+  };
+  PlatformDispatcher.instance.onError = (error, stack) {
+    Logger.error('Platform error: $error');
+    return true;
+  };
   try {
     await Firebase.initializeApp();
     Logger.info('Firebase initialized');
