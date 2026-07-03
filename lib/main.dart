183e5c5ff2fa7e3aec1597416e21890de49d4c25
abderrahimayadi08-dotcom <abderrahimayadi08@gmail.com>
2026-07-03 23:39:30 +0200

    Temp: minimal test screen without Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,7 @@
 import 'package:flutter/material.dart';
-import 'dart:ui';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:madrasa_app/app.dart';
-import 'package:madrasa_app/core/services/logger.dart';
+import 'package:madrasa_app/test_screen.dart';
 
-void main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  FlutterError.onError = (details) {
-    Logger.error('Flutter error: ${details.exception}');
-    FlutterError.presentError(details);
-  };
-  PlatformDispatcher.instance.onError = (error, stack) {
-    Logger.error('Platform error: $error');
-    return true;
-  };
-  try {
-    await Firebase.initializeApp();
-    Logger.info('Firebase initialized');
-  } catch (e) {
-    Logger.error('Firebase init failed: $e');
-  }
-  runApp(const MadrasaApp());
+  runApp(const TestScreen());
 }
